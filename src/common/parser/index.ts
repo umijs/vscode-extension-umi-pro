f4552d5e5bbb4a257fed5679fb3d7c17023877c3
DiamondYuan <admin@diamondyuan.com>
2019-04-29 15:44:43 +0800

    feat: support parseFile

--- src/common/parser/index.ts
+++ src/common/parser/index.ts
@@ -1,15 +1,29 @@
 import { IDvaModel } from './interface';
+import * as fs from 'mz/fs';
+import * as babelParser from '@babel/parser';
 
 interface IDvaModelParser {
   parse(code: string): Promise<IDvaModel>;
+
+  parseFile(path: string): Promise<IDvaModel>;
 }
 
 export class DvaModelParser implements IDvaModelParser {
-  public async parse(_code: string): Promise<IDvaModel> {
+  public async parse(code: string): Promise<IDvaModel> {
+    const ast = babelParser.parse(code, {
+      sourceType: 'module',
+      plugins: ['typescript', 'classProperties'],
+    });
+    console.log('ast', ast);
     return {
       namespace: 'Demo',
       effects: {},
       reducers: {},
     };
+  }
+
+  public async parseFile(path: string): Promise<IDvaModel> {
+    const code = await fs.readFile(path, 'utf-8');
+    return this.parse(code);
   }
 }
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,7 +1,11 @@
 import assert = require('assert');
+import * as path from 'path';
+import { DvaModelParser } from '../src/common/parser';
 
 describe('index.test.ts', () => {
-  it('hello world', () => {
-    assert(1 > 0);
+  it('test ', () => {
+    const userModelPath = path.resolve(__dirname, './fixture/model/user.js');
+    const userModel = new DvaModelParser().parseFile(userModelPath);
+    assert(userModel !== null);
   });
 });
