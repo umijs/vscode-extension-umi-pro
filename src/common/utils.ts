09ae175b7096cbd5e6f74c0d94339a3b2836d1a7
DiamondYuan <admin@diamondyuan.com>
2019-05-12 22:21:01 +0800

    feat: add getAllPages to common utils

--- src/common/utils.ts
+++ src/common/utils.ts
@@ -1,4 +1,4 @@
-import { join, resolve, dirname } from 'path';
+import { join, resolve, dirname, extname, basename } from 'path';
 import * as fs from 'mz/fs';
 import globby from 'globby';
 import { QuoteType, QuoteCharMap } from './config';
@@ -77,4 +77,35 @@ export function getProjectPath(
 export function isUndefined<T>(data: T | undefined): data is T {
   // eslint-disable-next-line no-undefined
   return data === undefined;
+}
+
+export async function getAllPages(cwd: string): Promise<string[]> {
+  const pages = (await globby(
+    [
+      `./**/*{${JS_EXT_NAMES.join(',')}}`,
+      `!./**/models/**/*{${JS_EXT_NAMES.join(',')}}`,
+      '!**/model.js',
+    ],
+    {
+      cwd,
+      deep: true,
+    }
+  )).filter(p =>
+    ['.d.ts', '.test.js', '.test.jsx', '.test.ts', '.test.tsx'].every(
+      ext => !p.endsWith(ext)
+    )
+  );
+
+  const pageSet = pages.reduce((set, page) => {
+    const ext = extname(page);
+
+    let pagePath = page.slice(0, -ext.length);
+    if (basename(pagePath) === 'index' && pagePath !== 'index') {
+      pagePath = dirname(pagePath);
+    }
+    set.add(pagePath);
+    return set;
+  }, new Set<string>());
+
+  return Array.from(pageSet);
 }
