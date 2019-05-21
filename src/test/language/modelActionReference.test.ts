f8903d04e104036672e86e3543992ca8dfc94607
DiamondYuan <admin@diamondyuan.com>
2019-05-21 11:45:58 +0800

    test: add test for ModelActionReference

---
+++ src/test/language/modelActionReference.test.ts
@@ -0,0 +1,52 @@
+import { Container } from 'typedi';
+import { workspace, Position, Location } from 'vscode';
+import { deepEqual } from 'assert';
+import { ModelActionReference } from '../../language/model';
+import { getAntdProFilePath } from '../utils';
+import {
+  VscodeServiceToken,
+  loadVscodeService,
+} from '../../services/vscodeService';
+
+describe('test ModelActionReference', async () => {
+  const provider = Container.get(ModelActionReference);
+  const vscodeService = Container.get(VscodeServiceToken);
+  await loadVscodeService(vscodeService);
+
+  function flattenLocation({
+    uri: { fsPath },
+    range: {
+      start: { line: startLine, character: startCharacter },
+      end: { line: endLine, character: endCharacter },
+    },
+  }: Location) {
+    return {
+      fsPath,
+      startLine,
+      startCharacter,
+      endLine,
+      endCharacter,
+    };
+  }
+
+  it('should get correct references', async function() {
+    this.timeout(10000);
+    const text = await workspace.openTextDocument(
+      getAntdProFilePath('src/pages/Profile/models/profile.js')
+    );
+    const references = await provider.provideReferences(
+      text,
+      new Position(20, 12)
+    );
+    deepEqual(references!.length, 1);
+    deepEqual(references!.map(flattenLocation), [
+      {
+        fsPath: getAntdProFilePath('src/pages/Profile/AdvancedProfile.js'),
+        startLine: 195,
+        startCharacter: 12,
+        endLine: 195,
+        endCharacter: 35,
+      },
+    ]);
+  });
+});
