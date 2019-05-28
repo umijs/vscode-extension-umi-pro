985aca67da1844c5ce3f7d801398e50143b1c62f
DiamondYuan <admin@diamondyuan.com>
2019-05-28 15:36:36 +0800

    fix: support project use dva-core

--- src/common/fileWatcher.ts
+++ src/common/fileWatcher.ts
@@ -20,7 +20,7 @@ async function needExtension(projectPath: string) {
   try {
     const packageJson = JSON.parse(await fs.readFile(packageJsonPath, { encoding: 'utf-8' }));
     const { dependencies = {} } = packageJson;
-    return !!(dependencies.umi || dependencies.dva);
+    return !!(dependencies.umi || dependencies.dva || dependencies['dva-core']);
   } catch (error) {
     logger.info(error);
     return false;
